4b96caf048f95e179e0127ab8fe3db5f496ce981
Ryann <ryann.hall2004@gmail.com>
2024-12-05 19:21:35 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+let employee = {
+    name: "Jane Doe",
+    streetAdress: "11226 Mistyhallow rd, chesterfield, VA"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    if (!employee || typeof employee !== "object") {
+        throw new TypeError("The first argument must be a valid object");
+    }
+    let newEmployee = {...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
